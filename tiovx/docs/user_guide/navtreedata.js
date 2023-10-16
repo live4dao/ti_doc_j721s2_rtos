c84acb8cbee86961a02312499c6b21ed841cc33d
BrandonChen <brandon752436@gmail.com>
2023-10-16 11:17:47 +0800

    rtos 09_00_00_02 doc

---
+++ tiovx/docs/user_guide/navtreedata.js
@@ -0,0 +1,64 @@
+/*
+@ @licstart  The following is the entire license notice for the
+JavaScript code in this file.
+
+Copyright (C) 1997-2017 by Dimitri van Heesch
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+@licend  The above is the entire license notice
+for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "TIOVX User Guide", "index.html", [
+    [ "Overview", "index.html", [
+      [ "Introduction", "index.html#autotoc_md7", null ],
+      [ "Directory Structure", "index.html#TIOVX_PACKAGE_CONTENTS", null ]
+    ] ],
+    [ "User Resources", "REFERENCES.html", "REFERENCES" ],
+    [ "Resources", "RESOURCES.html", [
+      [ "Khronos Pages", "RESOURCES.html#autotoc_md0", null ],
+      [ "Specs", "RESOURCES.html#autotoc_md1", null ],
+      [ "Tutorials/Presentations", "RESOURCES.html#autotoc_md2", null ],
+      [ "Papers", "RESOURCES.html#autotoc_md3", null ],
+      [ "FAQ's", "RESOURCES.html#autotoc_md4", null ],
+      [ "TI Support", "RESOURCES.html#autotoc_md5", null ]
+    ] ],
+    [ "TI Disclaimer", "TI_DISCLAIMER.html", [
+      [ "IMPORTANT NOTICE", "TI_DISCLAIMER.html#autotoc_md6", null ]
+    ] ],
+    [ "APIs", "modules.html", "modules" ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"BUILD_INSTRUCTIONS.html",
+"classtiovx_1_1enums_1_1InterpolationType.html#a6cd44a135647ca1a3926b8f0adeaee16",
+"classtiovx_1_1node_1_1NodeIntegralImage.html#a65529a3e4a7e07161f7405129062b4a0",
+"group__group__tivx__ext__host.html#gad7aa0fdded06205fb515f77c60e8cac7",
+"group__group__tivx__int__common__kernel.html#ga6a29e21c9b848b432e62f0e7d80cdeb7",
+"group__group__tivx__platform.html",
+"group__group__vision__function__vpac__ldc.html#ga7301559d598a563e52087a35b51d50fe",
+"group__group__vx__graph__cfg.html#ga328a381d97286ef5ef025be1e9471437",
+"group__group__vx__ti__extensions__cfg.html#gac307474d850020473abe32898aa1759c",
+"structtivx__graph__t.html#a2ef0064e0ff7014c853dbcf14cc56dee",
+"structtivx__obj__desc__node__t.html#aa3b9db6f7ef102ab5abe6658a9659d19",
+"structtivx__signature__t.html#a80f6496e8b7f46f488dc76b3adcda079"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
